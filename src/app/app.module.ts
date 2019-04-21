0435b14d03aa035021eec2192bfaaa7813ce5cb4
HardPocketRocket <rober3392@gmail.com>
2019-04-21 23:19:08 +0200

    added some basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -10,8 +11,28 @@ import { BookSearchComponent } from './book-search/book-search.component';
 import { DetailsService } from './Services/details.service';
 import { SearchService } from './Services/search.service';
 
+const routes: Routes = [
+  {
+    path: 'search',
+    component: BookSearchComponent
+  },
+  {
+    path: 'results',
+    component: BookSearchResultsComponent
+  },
+  {
+    path: 'details',
+    component: BookDetailsComponent
+  },
+  {
+    path: '',
+    component: BookSearchComponent
+  }
+];
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, RouterModule,RouterModule.forRoot(routes)],
+  exports: [RouterModule],
   declarations: [ AppComponent, HelloComponent, BookDetailsComponent, BookSearchResultsComponent, BookSearchComponent ],
   bootstrap:    [ AppComponent ],
   providers: [DetailsService, SearchService]
