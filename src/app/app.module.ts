c7f34732bdf2b0b5c5b585724a1ac9e868ee40bf
HardPocketRocket <rober3392@gmail.com>
2019-04-21 22:16:12 +0200

    Began work on the routing Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HelloComponent } from './hello.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { BookSearchResultsComponent } from './book-search-results/book-search-results.component';
 import { BookSearchComponent } from './book-search/book-search.component';
-import { DetailsServiceService } from './Services/details-service.service';
 import { DetailsService } from './Services/details.service';
 import { SearchService } from './Services/search.service';
 
@@ -15,6 +14,6 @@ import { SearchService } from './Services/search.service';
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, BookDetailsComponent, BookSearchResultsComponent, BookSearchComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [DetailsServiceService, DetailsService, SearchService]
+  providers: [DetailsService, SearchService]
 })
 export class AppModule { }
